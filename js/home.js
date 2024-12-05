5b4936958292aef260257f350045baec225b7597
mddalimmiah <mddalimsaif@gmail.com>
2024-12-05 22:23:25 +0600

    added update balance UI and DOM

--- js/home.js
+++ js/home.js
@@ -18,8 +18,33 @@ step-2: get money to be added to the account balance
 
     const addMoneyInput=document.getElementById('input-add-money').value;
     console.log(addMoneyInput)
-    // step-3: get the pin number:
+    //  get the pin number:
     const pinNUmberInput=document.getElementById('input-pin-number').value;
     console.log(pinNUmberInput)
+    // step-3: verify the pin number:
+    // this is wrong way to validate pin number (আমরা আপাতত এই ভাবে করছি পরবর্তীতে আমরা সঠিক ভাবে করবে।)
+     if(pinNUmberInput==='1234'){
+        console.log('Adding Money to Your Account');
+
+         //  step-4: get the current balance:
+
+    const balance=document.getElementById('account-balance').innerText;
+    // console.log( typeof balance);
+        //  step-5: add money input with balance and parsefloat money and balance:
+        const addMoneyNumber=parseFloat(addMoneyInput);
+        const balanceNumber=parseFloat(balance);
+        const newBalance=addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step-6: update the balance in the UI/DOM
+        // গেট করার জন্য  ভ্যারিএবল রাখবো বাট সেট করার জন্য ভ্যারিএবল এ রাখতে হবে না ডাইরেক্টে করা যাবে।
+         document.getElementById('account-balance').innerText=newBalance;
+
+     }
+     else{
+        alert('Failed to Add Money! Please Try Again!');
+     }
+
+   
     console.log('add money btn clicked')
  })
